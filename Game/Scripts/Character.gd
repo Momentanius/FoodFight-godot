f81db3069c198cd1ec66777cb452f049da5f9d0c
Momentanius <guilhermegustavogohr@gmail.com>
2019-04-11 15:28:50 -0300

    Dopller Effect e audio/som

--- Game/Scripts/Character.gd
+++ Game/Scripts/Character.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 const PROJECTILE_SPEED = 50
+var can_fire = true
 
 var ammo_types = {}
 
@@ -9,11 +10,18 @@ func _enter_tree(): #chamado quando entra no nó. vem antes de ready
 	randomize()
 
 func fire():
-	var random_bullet = ammo_types[randi() % ammo_types.size()]
-	var bullet = load(random_bullet).instance()
-	add_child(bullet)
-	bullet.set_as_toplevel(true) #Ignora as transformações dos parentes
-	bullet.global_transform = $Forward.global_transform
-	var character_forward = get_global_transform().basis[2].normalized()
-	bullet.set_linear_velocity(character_forward * PROJECTILE_SPEED)
-	bullet.add_collision_exception_with(self) #Evite colidir consigo mesmo.
+	if can_fire:
+		can_fire = false
+		$CanFire.start()
+		var random_bullet = ammo_types[randi() % ammo_types.size()]
+		var bullet = load(random_bullet).instance()
+		add_child(bullet)
+		bullet.set_as_toplevel(true) #Ignora as transformações dos parentes
+		bullet.global_transform = $Forward.global_transform
+		var character_forward = get_global_transform().basis[2].normalized()
+		bullet.set_linear_velocity(character_forward * PROJECTILE_SPEED)
+		bullet.add_collision_exception_with(self) #Evite colidir consigo mesmo.
+	
+
+func _on_CanFire_timeout():
+	can_fire = true
